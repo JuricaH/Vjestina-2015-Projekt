4664f031815064a8dc736aeeb88f22e2a55e9098
JuricaH <jurica.hanzek@fer.hr>
2016-01-31 18:45:12 +0100

    Nedovršeni projekt. Ne znam napraviti grafiku.

---
+++ Mlin/Mlin/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+
+namespace Mlin.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected Boolean isMlin()
+        {
+            return true;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Ucenje()
+        {
+            return View();
+        }
+
+        public ActionResult Igra()
+        {
+            
+            return View();
+        }
+    }
+}
